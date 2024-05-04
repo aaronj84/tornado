63fbe7eb4c4ea36309b9deebeae2e36dbda521bd
TheODDYSEY <mosessabz07@gmail.com>
2024-05-04 14:37:56 +0300

    created custom model ts 1:55:56

--- custom_postgres/models/example/film_ratings.sql
+++ custom_postgres/models/example/film_ratings.sql
@@ -1,33 +1,34 @@
 WITH films_with_ratings AS (
-    SELECT
+    SELECT 
         film_id,
         title,
         release_date,
         price,
         rating,
         user_rating,
-        CASE
-            WHEN CAST(rating AS NUMERIC) >= 4.5 THEN 'Excellent'
-            WHEN CAST(rating AS NUMERIC) >= 4.0 THEN 'Good'
-            WHEN CAST(rating AS NUMERIC) >= 3.0 THEN 'Average'
+        CASE 
+            WHEN user_rating::numeric >= 4.5 THEN 'Excellent'
+            WHEN user_rating::numeric >= 4.0 THEN 'Good'
+            WHEN user_rating::numeric >= 3.0 THEN 'Average'
             ELSE 'Poor'
         END AS rating_category
-    FROM {{ ref('films') }}  -- Using dbt's ref function to reference the films table
+    FROM films
 ),
+
 
 films_with_actors AS (
     SELECT
         f.film_id,
         f.title,
-        STRING_AGG(a.actor_name, ', ') AS actors  -- Aggregating actor names for each film
-    FROM {{ ref('films') }} f
-    LEFT JOIN {{ ref('film_actors') }} fa ON f.film_id = fa.film_id
-    LEFT JOIN {{ ref('actors') }} a ON fa.actor_id = a.actor_id
-    GROUP BY f.film_id, f.title
+        STRING_AGG(a.actor_name, ',') AS actors
+    FROM films f 
+    LEFT JOIN film_actors fa ON f.film_id = fa.film_id
+    LEFT JOIN actors a ON fa.actor_id = a.actor_id
+    GROUP BY f.film_id, f.title  
 )
 
-SELECT
-    fwf.*,
+SELECT 
+    fwr.*,
     fwa.actors
-FROM films_with_ratings fwf
-LEFT JOIN films_with_actors fwa ON fwf.film_id = fwa.film_id
+FROM films_with_ratings fwr
+JOIN films_with_actors fwa ON fwr.film_id = fwa.film_id
