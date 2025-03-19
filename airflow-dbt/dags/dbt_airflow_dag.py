2a1beba9aae5783741a62a2d84c70d3f5983ce43
Aaron Johnson <aaronj84@gmail.com>
2025-03-19 12:05:49 -0600

    Got everything just about running

---
+++ airflow-dbt/dags/dbt_airflow_dag.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 3, 19),
+    'catchup': False
+}
+
+dag = DAG(
+    'dbt_run_dag',
+    default_args=default_args,
+    schedule_interval='@daily',  # Runs once per day
+    catchup=False
+)
+
+dbt_run = BashOperator(
+    task_id='dbt_run',
+    bash_command='/home/airflow/.local/bin/dbt run',
+    dag=dag
+)
